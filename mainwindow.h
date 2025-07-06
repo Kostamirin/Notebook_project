33af893663e26113cffd58082d3f0f5a1d8bb9bf
kleymuner <kostavlasov834@gmail.com>
2025-07-06 22:12:14 +0300

    gitignore __update

--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,12 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+
+namespace Ui
+{
+    class MainWindow;
+}
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -18,4 +23,6 @@ public:
 private:
     Ui::MainWindow *ui;
 };
+
+
 #endif // MAINWINDOW_H
